15a5e0d0cdd5179328e0215d2eb4b25848d0ff36
Fedor <vailbi94@gmail.com>
2022-06-15 22:44:59 +0300

    comlpete task 16

---
+++ Задача 16/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите 1-ое число");
+int inputNumber1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2-ое число");
+int inputNumber2 = Convert.ToInt32(Console.ReadLine());
+
+
+if (Sqrt(inputNumber1) == inputNumber2 || Sqrt(inputNumber2) == inputNumber1)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+int Sqrt (int x)
+{
+    return x*x;
+}
