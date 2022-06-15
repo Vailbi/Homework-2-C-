dccf6104f5f9d1840542f078d80c558b1e96e790
Fedor <vailbi94@gmail.com>
2022-06-15 22:27:58 +0300

    comlpete task 10

--- Задача 10/Program.cs
+++ Задача 10/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿ Console.WriteLine("Введите трехзначное число");
+ 
+ int num = Convert.ToInt32(Console.ReadLine());
+ int result = num%100;                     
+ int result2 = result/10;
+Console.WriteLine(result2);
+
+
+ //string array = Console.ReadLine();          // *вывод третьего числа через массив*
+ //Console.WriteLine(array[1]);
