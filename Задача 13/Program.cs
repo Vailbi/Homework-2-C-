e0cbac72b657aefd24bf50755227dc561f7eb3e5
Fedor <vailbi94@gmail.com>
2022-06-15 22:33:16 +0300

    comlpete task 13

---
+++ Задача 13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int inputNumber = Convert.ToInt32(Console.ReadLine());  // **нахождение 3 числа**
+
+if (inputNumber < 100)
+{
+    Console.WriteLine("Третий цифры нет");
+}
+else
+{
+    
+    while(inputNumber > 999)
+    {
+        inputNumber = inputNumber/10;
+
+    }
+ 
+Console.WriteLine(inputNumber%10);
+}
