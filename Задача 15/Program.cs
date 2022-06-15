cbdffe4a4b46e07b90b679acb0eade6e2e4ee1ab
Fedor <vailbi94@gmail.com>
2022-06-15 22:41:20 +0300

    comlpete task 15

---
+++ Задача 15/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int inputNumber = Convert.ToInt32(Console.ReadLine()); // Проверка дней недели
+if (inputNumber ==6 || inputNumber ==7)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
